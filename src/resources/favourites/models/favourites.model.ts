0daac58c922064b2f07ebe3ca8adfba0e3ac2349
Nikolai Makarevich <nikoladze529@mail.ru>
2022-07-19 12:33:48 +0300

    refactor: separate db and response models

--- src/resources/favourites/models/favourites.model.ts
+++ src/resources/favourites/models/favourites.model.ts
@@ -3,6 +3,12 @@ import { AlbumModel } from '../../album/models/album.model';
 import { TrackModel } from '../../track/models/track.model';
 
 export interface FavouritesModel {
+  artists: string[];
+  albums: string[];
+  tracks: string[];
+}
+
+export interface FavouritesResponseModel {
   artists: ArtistModel[];
   albums: AlbumModel[];
   tracks: TrackModel[];
