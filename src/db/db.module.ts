4ea61d3f4a5f1c92d9a9591a9f3f96102a354005
Nikolai Makarevich <nikoladze529@mail.ru>
2022-07-26 15:18:49 +0300

    refactor: delete in-memory db

--- src/db/db.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { InMemoryDb } from './in-memory.db';
-
-@Module({
-  providers: [InMemoryDb],
-  exports: [InMemoryDb],
-})
-export class DbModule {}
