b9e9cedc65b11972f833d0a62b52dc8620ecf5d1
Nikolai Makarevich <nikoladze529@mail.ru>
2022-08-06 00:15:54 +0300

    refactor: move code to separated function

--- src/logger/custom-logger.service.ts
+++ src/logger/custom-logger.service.ts
@@ -7,28 +7,8 @@ import 'dotenv/config';
 export class CustomLogger extends ConsoleLogger {
   private fileSize = parseInt(process.env.LOG_FILE_SIZE_KB) * 1024;
   customLog = async (message: string): Promise<void> => {
-    await mkdir(path.join(process.cwd(), 'logs/logs'), { recursive: true });
-    const dirname = path.resolve(process.cwd(), 'logs/logs');
-
-    const ls = await readdir(dirname);
-    let order = ls.length === 0 ? 0 : ls.length - 1;
-    let filename = ls[ls.length - 1] || createFileName('log', order);
-    if (ls.length === 0) {
-      await createFile(dirname, filename);
-    }
-
-    const { size } = await stat(path.resolve(dirname, filename));
-    const messageSize = Buffer.byteLength(message, 'utf-8') + 23;
-
-    if (size + messageSize >= this.fileSize) {
-      order += 1;
-      filename = createFileName('log', order);
-      await createFile(dirname, filename);
-    }
-
-    const date = new Date().toLocaleString();
+    await saveLog('log', message, this.fileSize);
     this.log(message);
-    await appendFile(path.resolve(dirname, filename), `${date} - ${message}\n`);
   };
 
   customError(message: any, ...optionalParams: any[]): any {
@@ -40,7 +20,34 @@ export class CustomLogger extends ConsoleLogger {
   }
 }
 
-function createFileName(name: string, order: number) {
+async function saveLog(name: string, message: string, fileSize: number) {
+  await mkdir(path.join(process.cwd(), `logs/${name}s`), { recursive: true });
+  const dirname = path.resolve(process.cwd(), `logs/${name}s`);
+
+  const ls = await readdir(dirname);
+  let order = ls.length === 0 ? 0 : ls.length - 1;
+  let filename = ls[ls.length - 1] || generateFileName(name, order);
+  if (ls.length === 0) {
+    await createFile(dirname, filename);
+  }
+
+  const { size } = await stat(path.resolve(dirname, filename));
+  const dateSize = Buffer.byteLength(new Date().toLocaleString(), 'utf-8');
+  const messageSize = Buffer.byteLength(message, 'utf-8');
+
+  if (size + messageSize + dateSize >= fileSize) {
+    order += 1;
+    filename = generateFileName('log', order);
+    await createFile(dirname, filename);
+  }
+
+  await appendFile(
+    path.resolve(dirname, filename),
+    `${new Date().toLocaleString()} - ${message}\n`,
+  );
+}
+
+function generateFileName(name: string, order: number) {
   return order === 0 ? `${name}.txt` : `${name}${order}.txt`;
 }
 
